df2567f304e27f9c260779e815e09d6db562e4f9
Cédric Bagot <56221981+Cedricbgt@users.noreply.github.com>
2025-01-30 17:32:02 +0100

    Update test.php

--- 1/test.php
+++ 1/test.php
@@ -4,20 +4,12 @@ require_once('../_helpers/strip.php');
 
 // https://depthsecurity.com/blog/exploitation-xml-external-entity-xxe-injection
 
-$xml = isset($_GET['xml']) && strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
+libxml_disable_entity_loader(true);
+
+$xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
+$document->loadXML($xml);
+$parsedDocument = simplexml_import_dom($document);
 
-// Use LIBXML_NOENT with caution and validate input
-libxml_use_internal_errors(true);
-
-if (!$document->loadXML($xml, LIBXML_NOENT | LIBXML_NOERROR | LIBXML_NOWARNING)) {
-    echo "Error loading XML";
-    foreach (libxml_get_errors() as $error) {
-        echo "\n", $error->message;
-    }
-    libxml_clear_errors();
-} else {
-    $parsedDocument = simplexml_import_dom($document);
-    echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8'); // Sanitize output to prevent XSS
-}
+echo $parsedDocument->content;
