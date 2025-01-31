ebaa8a336aee9d470e8e0eef3bf7f7fc4dc6bb56
Cédric Bagot <56221981+Cedricbgt@users.noreply.github.com>
2025-01-31 08:02:01 +0100

    Update XmlReader_Tests.cs

--- 1/XmlReader_Tests.cs
+++ 1/XmlReader_Tests.cs
@@ -16,18 +16,20 @@ namespace XXEExamples.Tests
             AssertXXE.IsXMLParserSafe((string xml) =>
             {
                 XmlReaderSettings settings = new XmlReaderSettings();
-                settings.DtdProcessing = DtdProcessing.Parse;
-                settings.XmlResolver = new XmlUrlResolver();
+                settings.DtdProcessing = DtdProcessing.Prohibit; // Disable DTD processing
+                settings.XmlResolver = null; // Disable external entity resolution
                 settings.MaxCharactersFromEntities = 6000;
 
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
                 {
                     XmlReader reader = XmlReader.Create(stream, settings);
-
-                    var xmlDocument = new XmlDocument();
-                    xmlDocument.XmlResolver = new XmlUrlResolver();
-                    xmlDocument.Load(reader);
-                    return xmlDocument.InnerText;
+                    while (reader.Read())
+                    {
+                        var xmlDocument = new XmlDocument();
+                        xmlDocument.XmlResolver = new XmlUrlResolver();
+                        xmlDocument.Load(reader);
+                        return xmlDocument.InnerText;
+                    }
                 }
             }, false);
         }
